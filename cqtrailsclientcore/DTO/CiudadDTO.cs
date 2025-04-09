7ff78d356294ec74f09a9d5e60b1c8ac2c598b04
guelo0987 <jessielopez0987@gmail.com>
2025-04-08 23:52:49 -0400

    push

---
+++ cqtrailsclientcore/DTO/CiudadDTO.cs
@@ -0,0 +1,8 @@
+namespace cqtrailsclientcore.DTO;
+
+public class CiudadDTO
+{
+    public int IdCiudad { get; set; }
+    public string Nombre { get; set; }
+    public string Estado { get; set; }
+} 
